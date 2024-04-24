260af817c0ec58cee42780134306baca23b6cf78
Pierre A DRAMAIX <2310pidramaix@student.epfc.eu>
2024-04-24 19:11:49 +0200

    push to present to teacher

--- prbd_2324_g01/ViewModel/TricountsViewModel.cs
+++ prbd_2324_g01/ViewModel/TricountsViewModel.cs
@@ -10,11 +10,15 @@ using System.Windows.Input;
 namespace prbd_2324_g01.ViewModel;
 
 public class TricountsViewModel : ViewModelCommon {
+    
 
     private ObservableCollection<Tricount> _tricounts;
     public ObservableCollection<Tricount> Tricounts {
         get => _tricounts;
-        set => SetProperty(ref _tricounts, value);
+        set {
+            SetProperty(ref _tricounts, value);
+            RaisePropertyChanged(nameof(Tricounts));
+        } 
     }
 
     private string _filter;
@@ -22,14 +26,34 @@ public class TricountsViewModel : ViewModelCommon {
         get => _filter;
         set => SetProperty(ref _filter, value, ApplyFilterAction);
     }
+    
+    public ICommand ClearFilter { get; set; }
     public ObservableCollection<TricountDetailViewModel> TricountsDetailVM { get; set; } = new ();
     
     public ICommand DisplayTricountDetails { get; set; }
     private void ApplyFilterAction() {
+        
+        IEnumerable<Tricount> query = Tricounts;
+        
+        if (!string.IsNullOrEmpty(Filter)) {
+            query = from t in Tricounts
+                where t.Title.Contains(Filter)
+                      || t.Description.Contains(Filter)
+                      || User.UserById(t.Creator).FullName.Contains(Filter)
+                      || t.Subscribers.Any(sub => sub.FullName.Contains(Filter))
+                      || t.Operations.Any(ope => ope.Title.Contains(Filter))
+                select t;
+        }
+
+        Tricounts = new ObservableCollection<Tricount>(query);
+
+        foreach (var tricount in Tricounts) {
+            Console.WriteLine(tricount.Title);
+        }
+
 
 
     }
-
     private void InitiateVM() {
         IEnumerable<Tricount> tricounts = Tricount.tricountByMember(CurrentUser);
 
@@ -42,8 +66,10 @@ public class TricountsViewModel : ViewModelCommon {
 
 
     public TricountsViewModel() {
-        ApplyFilterAction();
+        
+        ClearFilter = new RelayCommand(() => Filter = "");
         InitiateVM();
+        ApplyFilterAction();
 
         DisplayTricountDetails = new RelayCommand<TricountDetailViewModel>(vm => {
             NotifyColleagues(App.Messages.MSG_DISPLAY_TRICOUNT, vm.Tricount);
